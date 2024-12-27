49bb3390ca4805b334ad7c0b69d134bd079c3a05
finddme <53667002+finddme@users.noreply.github.com>
2024-12-27 14:38:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 pip install crawl4ai
 pip install playwright
 playwright install
+playwight install chrome
 crawl4ai-setup
 playwright install-deps
 pip install nest-asyncio
-playwright install chrome
